4598695a94cded999b7baa617c7705e068107931
Bu1Men-Git <vereskov.rom@gmail.com>
2023-10-22 14:28:03 +0300

    Updates

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import * as fs from 'fs';
 import Gameboy from 'serverboy';
 import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 6969 });
-const buttons = {
-    "A": Gameboy.KEYMAP.A,
-    "B": Gameboy.KEYMAP.B,
-    "R": Gameboy.KEYMAP.RIGHT,
-    "L": Gameboy.KEYMAP.LEFT,
-    "U": Gameboy.KEYMAP.DOWN,
-    "D": Gameboy.KEYMAP.UP,
-    "SL": Gameboy.KEYMAP.SELECT,
-    "ST": Gameboy.KEYMAP.START
-};
+const buttons = [
+    { "A": Gameboy.KEYMAP.A },
+    { "B": Gameboy.KEYMAP.B },
+    { "R": Gameboy.KEYMAP.RIGHT },
+    { "L": Gameboy.KEYMAP.LEFT },
+    { "U": Gameboy.KEYMAP.DOWN },
+    { "D": Gameboy.KEYMAP.UP },
+    { "SL": Gameboy.KEYMAP.SELECT },
+    { "ST": Gameboy.KEYMAP.START }
+];
 const roms = [
     'pokemon_emerald.gba',
     'pokemon_crystal.gbc',
@@ -27,17 +27,19 @@ function loadGame(gb, game, sav) {
         gb.loadRom(rom);
 }
 wss.on('connection', async function connection(ws) {
+    var ready = false;
     const gb = new Gameboy();
-    loadGame(gb, 2);
     var interval_id2 = setInterval(() => {
+        if (!ready)
+            return;
         gb.doFrame();
     }, 1000 / 120);
     var interval_id1 = setInterval(() => {
-        // resolution = {w: 160, h: 144}
+        if (!ready)
+            return;
         var pix = gb.getScreen();
         var str = "scrn" + Buffer.from(pix, 'ascii');
         ws.send(str);
-        process.exit();
     }, 1000 / 30);
     ws.on('message', (data) => {
         var dat = data.toString();
@@ -51,7 +53,8 @@ wss.on('connection', async function connection(ws) {
                     break;
                 case 'load':
                     var strs = dat.split('|');
-                    gb.loadGame(Number.parseInt(strs[1]), strs[2]);
+                    loadGame(gb, Number.parseInt(strs[1]), strs[2]);
+                    ready = true;
                     break;
             }
         }
